22f76ad60aefe0b6c333979b39a2cf883de980c1
seafood0511 <114842813+seafood0511@users.noreply.github.com>
2022-10-02 10:43:11 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,39 @@
+basic.showString("2D25 task 2")
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+})
